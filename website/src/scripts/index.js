59647f838de1a91002f9c89888023c01ffa303be
Darin Buzon <dabuzon@Darins-MacBook-Pro.local>
2019-01-11 11:11:04 -0800

    Change file structure

---
+++ website/src/scripts/index.js
@@ -0,0 +1,10 @@
+/* --- LIBRARIES --- */
+import 'jquery';
+import 'dayjs';
+import 'feather-icons';
+
+/* --- ASSETS --- */
+import '../styles/main.scss';
+import '../index.html';
+
+/* --- MODULES --- */
